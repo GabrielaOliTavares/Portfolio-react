d1fa58e0865e98ef59b8d2e8801841e82f2705ab
GabrielaOliTavares <145030166+GabrielaOliTavares@users.noreply.github.com>
2023-12-20 18:59:59 +0000

    $ git commit

--- portfolio/src/pages/Projetos/index.jsx
+++ portfolio/src/pages/Projetos/index.jsx
@@ -1,15 +1,14 @@
-import Header from '../../components/Header'
-import Footer from '../../components/Footer'
-import Container from '../../components/Container'
+
+
+import Card from '../../components/Card'
 
 function Projetos() {
     return (
         <>
-            <Header />
-            <Container>
-                <h2>Projetos</h2>
-            </Container>
-            <Footer />
+            <h2>Projetos</h2>
+            <Card />
+            <Card />
+            <Card />
         </>
     )
 }
--- portfolio/src/routes.jsx
+++ portfolio/src/routes.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Projetos from './pages/Projetos'
 import Sobre from './pages/Sobre'
+import Projetos from './pages/Projetos'
 import Contatos from './pages/Contatos'
 import Page404 from './pages/Page404'
+import PageBase from './pages/PageBase'
 
 function AppRoutes() {
     return (
         <BrowserRouter>
             <Routes>
-            
-    <Route path="/" element={ <Home /> }></Route>
-    <Route path="/sobre" element={ <Sobre /> }></Route>
-    <Route path="/projetos" element={ <Projetos /> }></Route>
-    <Route path="/contatos" element={ <Contatos /> }></Route>
-    <Route path="*" element={ <Page404 /> }></Route>
-</Routes>
-            
-    </BrowserRouter>
+                <Route path='/' element={ <PageBase /> }>
+                    <Route index element={ <Home /> }></Route>
+                    <Route path="/sobre" element={ <Sobre /> }></Route>
+                    <Route path="/projetos" element={ <Projetos /> }></Route>
+                    <Route path="/contatos" element={ <Contatos /> }></Route>
+                    <Route path="*" element={ <Page404 /> }></Route>
+                </Route>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
